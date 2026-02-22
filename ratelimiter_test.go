f21e21510ff72cd5f6ddd90fcf05b166460863ec
Personal - Stakshi <personal-stakshi@Stakshis-MacBook-Air.local>
2026-02-22 18:07:14 +0530

    test: add concurrency test

--- ratelimiter_test.go
+++ ratelimiter_test.go
@@ -1,6 +1,10 @@
 package ratelimiter
 
-import "testing"
+import (
+	"testing"
+	"sync"
+	"sync/atomic"
+)
 
 func TestTokenBucket(t *testing.T) {
 	tb := NewTokenBucket(2, 1)
@@ -17,3 +21,27 @@ func TestTokenBucket(t *testing.T) {
 		t.Fatal("Expected third request to be blocked")
 	}
 }
+
+func TestTokenBucketConcurrent(t *testing.T) {
+	tb := NewTokenBucket(100, 0)
+
+	var wg sync.WaitGroup
+	allowed := int64(0)
+
+	for range 200 {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if tb.Allow() {
+				atomic.AddInt64(&allowed, 1)
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	if allowed != 100 {
+		t.Fatalf("Expected 100 allowed, got %d", allowed)
+	}
+}
+
