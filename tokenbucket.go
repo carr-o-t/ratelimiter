2c2dc859efe6f354efa462aba86274e175c54142
Personal - Stakshi <personal-stakshi@Stakshis-MacBook-Air.local>
2026-02-22 18:07:02 +0530

    -

--- tokenbucket.go
+++ tokenbucket.go
@@ -6,44 +6,47 @@ import (
 )
 
 type TokenBucket struct {
-    capacity   int64		// max tokens
-    tokens     int64		// current tokens
-    refillRate int64		// tokens per second
-    lastRefill time.Time	// last refill time (nanosec)
-    mu         sync.Mutex
+	capacity   int64     // max tokens
+	tokens     int64     // current tokens
+	refillRate int64     // tokens per second
+	lastRefill time.Time // last refill time (nanosec)
+	mu         sync.Mutex
 }
 
-func NewTokenBucket(capacity int64, refillRate int64) *TokenBucket  {
+func NewTokenBucket(capacity int64, refillRate int64) *TokenBucket {
 	return &TokenBucket{
 		capacity:   capacity,
-		tokens:     capacity,	// initial tokens, full capacity
+		tokens:     capacity, // initial tokens, full capacity
 		refillRate: refillRate,
 		lastRefill: time.Now(),
 	}
 }
 
 // private func
-func (tb * TokenBucket) refill() {
+func (tb *TokenBucket) refill() {
 	now := time.Now()
 	elapsed := now.Sub(tb.lastRefill).Seconds()
-	newTokens := int64(elapsed) * tb.refillRate
+	newTokens := int64(elapsed * float64(tb.refillRate))
 
 	if newTokens > 0 {
-		tb.tokens = min(tb.capacity, tb.tokens + newTokens)
+		tb.tokens += newTokens
+		if tb.tokens > tb.capacity {
+			tb.tokens = tb.capacity
+		}
+		tb.lastRefill = now
 	}
 
-	tb.lastRefill = now
 }
 
 // public
-func (tb * TokenBucket) Allow() bool {
+func (tb *TokenBucket) Allow() bool {
 	tb.mu.Lock()
 	defer tb.mu.Unlock()
 
 	tb.refill()
 
-	if(tb.tokens > 0) {
-		tb.tokens --
+	if tb.tokens > 0 {
+		tb.tokens--
 		return true
 	}
 
